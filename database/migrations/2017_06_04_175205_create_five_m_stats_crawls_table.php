7ce43f7f02374f570eeeee1b334d1a548c128050
Gregor Waldvogel <gregor@waldvogel.io>
2017-06-04 20:00:30 +0200

    first commit

---
+++ database/migrations/2017_06_04_175205_create_five_m_stats_crawls_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFiveMStatsCrawlsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('five_m_stats_crawls', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('playerCount');
+            $table->integer('serverCount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('five_m_stats_crawls');
+    }
+}
