b77ef210854ec6c79d98b5be802bba1b6fe216f4
Gregor Waldvogel <gregor@waldvogel.io>
2017-06-05 03:20:59 +0200

    fancy ui shit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -94,9 +94,7 @@
             '{{ $country->country }}',
             @endforeach],
         colorscale: [
-            [0,'rgb(5, 10, 172)'],[0.35,'rgb(40, 60, 190)'],
-            [0.5,'rgb(70, 100, 245)'], [0.6,'rgb(90, 120, 245)'],
-            [0.7,'rgb(106, 137, 247)'],[1,'rgb(220, 220, 220)']],
+            [0,'rgb(31, 119, 180)'],[0.5,'rgb(88,143,182)'],[0.95, 'rgb(156,182,200)'],[1,'rgb(255, 255, 255)'],],
         autocolorscale: false,
         reversescale: true,
         marker: {
@@ -105,12 +103,10 @@
                 width: 0.5
             }
         },
-        tick0: 0,
         zmin: 0,
-        dtick: 1000,
         colorbar: {
-            autotic: false,
-            title: 'Active FiveM Players'
+            autotic: true,
+            title: 'Active FiveM servers'
         }
     }];
 
@@ -144,9 +140,7 @@
             '{{ $country->country }}',
             @endforeach],
         colorscale: [
-            [0,'rgb(5, 10, 172)'],[0.35,'rgb(40, 60, 190)'],
-            [0.5,'rgb(70, 100, 245)'], [0.6,'rgb(90, 120, 245)'],
-            [0.7,'rgb(106, 137, 247)'],[1,'rgb(220, 220, 220)']],
+            [0,'rgb(255, 127, 14)'],[0.5,'rgb(255,185,124)'],[0.99, 'rgb(255,218,185)'],[1,'rgb(255, 255, 255)']],
         autocolorscale: false,
         reversescale: true,
         marker: {
@@ -159,8 +153,8 @@
         zmin: 0,
         dtick: 1000,
         colorbar: {
-            autotic: false,
-            title: 'Active FiveM Players'
+            autotic: true,
+            title: 'Active FiveM players'
         }
     }];
 
