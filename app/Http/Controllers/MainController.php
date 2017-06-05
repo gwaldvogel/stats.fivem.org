ef8bc9ee6527222da6e1e00dcec64cce6b162a0f
Gregor Waldvogel <gregor@waldvogel.io>
2017-06-05 04:15:41 +0200

    caching

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,20 +6,27 @@ use App\CountryStats;
 use App\FiveMStatsCrawl;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class MainController extends Controller
 {
     public function index()
     {
-        $fivemStatsLastH = FiveMStatsCrawl::where('updated_at', '>', Carbon::now()->subHours(1))->get();
+        $fivemStatsLastH = Cache::remember('fivemstatscrawl', 5, function() {
+           return FiveMStatsCrawl::where('updated_at', '>', Carbon::now()->subHours(1))->get();
+        });
 
-        $countryStatsServer = CountryStats::where('servers', '=', true)
-            ->orderBy('updated_at', 'desc')
-            ->first();
+        $countryStatsServer = Cache::remember('countrystatsserver', 5, function() {
+            return CountryStats::where('servers', '=', true)
+                ->orderBy('updated_at', 'desc')
+                ->first();
+        });
 
-        $countryStatsPlayers = CountryStats::where('servers', '=', false)
-            ->orderBy('updated_at', 'desc')
-            ->first();
+        $countryStatsPlayers = Cache::remember('countrystatsplayers', 5, function() {
+            return CountryStats::where('servers', '=', false)
+                ->orderBy('updated_at', 'desc')
+                ->first();
+        });
 
         return view('index', [
             'FiveMLastHour' => $fivemStatsLastH,
