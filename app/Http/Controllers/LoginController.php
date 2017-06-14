76a85c1c142c2dfec03b86879c4d98b0c93157ef
Gregor Waldvogel <gregor@waldvogel.io>
2017-06-14 05:25:50 +0200

    forgot some stuff for the login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class LoginController extends Controller
+{
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/dashboard');
+    }
+
+    /**
+     * Redirect the user to the Steam authentication page.
+     *
+     * @return Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::driver('steam')->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return Response
+     */
+    public function handleProviderCallback()
+    {
+        try {
+            $user = Socialite::driver('steam')->user();
+            $authUser = $this->findOrCreateUser($user);
+            Auth::login($authUser, true);
+            return redirect('/dashboard');
+        } catch (Exception $e) {
+            return redirect('/login');
+        }
+    }
+
+    private function findOrCreateUser($steamUser)
+    {
+        if ($authUser = User::where('steam_id', $steamUser->id)->first()) {
+            return $authUser;
+        }
+
+        return User::create([
+            'nickname' => $steamUser->nickname,
+            'steam_id' => $steamUser->id,
+            'avatar' => $steamUser->avatar
+        ]);
+    }
+}
---
+++ app/User.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+
+class User extends Model implements AuthenticatableContract
+{
+    protected $fillable = [
+        'nickname',
+        'steam_id',
+        'avatar',
+    ];
+
+    public function getAuthIdentifierName()
+    {
+        return 'steam_id';
+    }
+
+    /**
+     * Get the unique identifier for the user.
+     *
+     * @return mixed
+     */
+    public function getAuthIdentifier()
+    {
+        return $this->steam_id;
+    }
+
+    /**
+     * Get the password for the user.
+     *
+     * @return string
+     */
+    public function getAuthPassword()
+    {
+        return null;
+    }
+
+    /**
+     * Get the token value for the "remember me" session.
+     *
+     * @return string
+     */
+    public function getRememberToken()
+    {
+        return null;
+    }
+
+    /**
+     * Set the token value for the "remember me" session.
+     *
+     * @param  string $value
+     * @return void
+     */
+    public function setRememberToken($value)
+    {
+        $this->remember_token = $value;
+    }
+
+    /**
+     * Get the column name for the "remember me" token.
+     *
+     * @return string
+     */
+    public function getRememberTokenName()
+    {
+        return 'remember_token';
+    }
+}
