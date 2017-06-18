c856f4b6a1a005692769f1c7a6e6d740720e0ace
Gregor Waldvogel <gregor@waldvogel.io>
2017-06-18 10:18:06 +0200

    Fixed player search

--- resources/views/shared/alerts.blade.php
+++ resources/views/shared/alerts.blade.php
@@ -1,20 +1,28 @@
 @if(Session::has('alert-success'))
-    <div class="alert alert-success" role="alert">
-        {{ Session::get('alert-success') }}
+    <div class="container-fluid">
+        <div class="alert alert-success" role="alert">
+            {{ Session::get('alert-success') }}
+        </div>
     </div>
 @endif
 @if(Session::has('alert-info'))
-    <div class="alert alert-info" role="alert">
-        {{ Session::get('alert-info') }}
+    <div class="container-fluid">
+        <div class="alert alert-info" role="alert">
+            {{ Session::get('alert-info') }}
+        </div>
     </div>
 @endif
 @if(Session::has('alert-warning'))
-    <div class="alert alert-warning" role="alert">
-        {{ Session::get('alert-warning') }}
+    <div class="container-fluid">
+        <div class="alert alert-warning" role="alert">
+            {{ Session::get('alert-warning') }}
+        </div>
     </div>
 @endif
 @if(Session::has('alert-danger'))
-    <div class="alert alert-danger" role="alert">
-        {{ Session::get('alert-danger') }}
+    <div class="container-fluid">
+        <div class="alert alert-danger" role="alert">
+            {{ Session::get('alert-danger') }}
+        </div>
     </div>
 @endif
